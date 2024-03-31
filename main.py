453c95049a1542c504855248758282f757d8a0b1
Usama Derbashi <usama.derbashi@trilobit.si>
2024-03-31 11:36:18 +0200

    added main

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-a = int(input("Give me a number: "))
-b = int(input("Give me another number: "))
-print(f"{a} + {b} = {a + b}")
+def main():
+	a = int(input("Give me a number: "))
+	b = int(input("Give me another number: "))
+	print(f"{a} + {b} = {a + b}")
+
+if __name__ == "__main__":
+	main()
