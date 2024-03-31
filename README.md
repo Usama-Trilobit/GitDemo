9ecabe012948aef47f5bccdbc4ff70875b2b9db2
Usama Derbashi <usama.derbashi@trilobit.si>
2024-03-31 11:34:00 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# this is for the purouse of demonstrating the manual
---
+++ main.py
@@ -0,0 +1,3 @@
+a = int(input("Give me a number: "))
+b = int(input("Give me another number: "))
+print(f"{a} + {b} = {a + b}")
