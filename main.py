5152bcc45b8829afd4611df1be32c46c06b036d8
Usama Derbashi <usama.derbashi@trilobit.si>
2024-03-31 12:00:26 +0200

    Revert "merging Dev into Main"

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-def main():
-	a = int(input("Give me a number: "))
-	b = int(input("Give me another number: "))
-	operator = input("what to do with the numbers? (a for add, m for multiply): ")
-
-	if operator == "a":
-		print(f"{a} + {b} = {a + b}")
-	elif operator == "m":
-		print(f"{a} * {b} = {a * b}")
-
-if __name__ == "__main__":
-	main()
+a = int(input("Give me a number: "))
+b = int(input("Give me another number: "))
+print(f"{a} + {b} = {a + b}")
